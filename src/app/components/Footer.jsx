ad8d6345101872d05fc8ff8f0dbf798f58532519
manarudesu <manalerdenebaater597@gmail.com>
2024-02-21 15:13:18 +0800

    its me

---
+++ src/app/components/Footer.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Footer = () => {
+    return (
+        <footer className='footer border border-top-[#333535] border-l-transparent border-r-transparent text-white opacity-60'>
+            <div className='container p-12 flex justify-between'>
+                <spa > AILT </spa>
+                <p className='text-slate-600'> All rights reversed. </p>
+            </div>
+        </footer>
+    )
+}
+
+export default Footer
